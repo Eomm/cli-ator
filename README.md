b169bc72eb331c9d40ad9651397eebb58be87265
Manuel Spigolon <behemoth89@gmail.com>
2019-06-13 21:20:02 +0200

    defining signature

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # cli-bone
 A cli tool to build cli tools FASTER
+
+
+```js
+const cli = require('./lib/index')
+
+const insta = cli({
+  help: 'man/help.txt',
+  helpArg: 'help',
+  commandsHelp: '',
+  commandsPath: 'command/',
+  commands: [
+    {
+      command: 'demo',
+      help: 'README.md',
+      helpArg: 'asd',
+      handler: (args)=>{
+        console.log('asd');     
+      }
+    }
+  ]
+})
+
+insta.on('start', (command, args) => {
+
+})
+
+insta.on('end', (command) => {
+
+})
+
+insta.on('error', (error, cmd) => {
+
+})
+
+insta.execute(process.argv.splice(2))
+```
--- test/cli.test.js
+++ test/cli.test.js
@@ -1,6 +1,47 @@
 'use strict'
 
-const t = require('tap')
+const { test } = require('tap')
 
-t.plan(1)
-t.pass()
+const cli = require('./lib/index')
+
+test('should apply default parameter', t => {
+  t.plan(2)
+
+  const insta = cli()
+
+  insta.on('start', (command, args) => {
+  })
+
+  insta.on('end', (command) => {
+  })
+
+  insta.on('error', (error) => {
+  })
+
+  insta.execute([])
+})
+
+test('should run all the events', t => {
+  t.plan(2)
+
+  const insta = cli()
+
+  insta.on('start', (command, args) => {
+  })
+
+  insta.on('end', (command) => {
+  })
+
+  insta.on('error', (error) => {
+  })
+
+  insta.execute([])
+  insta.execute([])
+})
+
+test('should throw if the execute parameter is not an array', t => {
+  t.plan(2)
+
+  const insta = cli()
+  insta.execute([])
+})
